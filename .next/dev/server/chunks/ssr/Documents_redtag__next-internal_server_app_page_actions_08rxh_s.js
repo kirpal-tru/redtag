7aa8f4d57c6b784faf6f7e9c02c2ba5b80a0a71d
kirpal-tru <51973101+tru-team@users.noreply.github.com>
2026-04-23 17:49:27 +0530

    Initial commit

--- .next/dev/server/chunks/ssr/Documents_redtag__next-internal_server_app_page_actions_08rxh_s.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/Documents/redtag/.next-internal/server/app/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=Documents_redtag__next-internal_server_app_page_actions_08rxh_s.js.map
--- .next/dev/static/chunks/Documents_redtag_src_app_page_tsx_0j~8xp5._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/Documents_redtag_src_components_0cm9b2r._.js","static/chunks/0h-__0u0~0oa._.js"],
-    source: "dynamic"
-});
